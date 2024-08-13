435e9646382b055857e2fc0288db0022076e5a81
Gopal Chandra Bera <gopal.bera@quantuminventions.com>
2024-08-13 14:22:20 +0530

    Release v0.0.4 (#5)
    * Initial implementation of git testing app (#1)
    
    * feat: add source file
    
    * feat: Add initial implementation
    
    * Add function for addition and add multi-thread security (#2)
    
    * feat: Add add() funtion
    
    * fix: Add multi-thread security
    
    * Add Subtract function and make it threadsafe (#4)
    
    * feat: Add subtract() function
    
    * fix: make subtract function thread safe

--- MyAddition.cpp
+++ MyAddition.cpp
@@ -2,12 +2,22 @@
 
 mutex addMutex;
 
+mutex subMutex;
+
 int add(int a, int b)
 {
 	addMutex.lock();
 	int sum = (a+b);
 	addMutex.unlock();
 	return sum;
+}
+
+int subtract(int a, int b)
+{
+	subMutex.lock();
+	int result = (a - b);
+	subMutex.unlock();
+	return result;
 }
 
 int main()
